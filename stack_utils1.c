33ae3a184261cc4072a578cd941a0109cde8ca05
Yi Lin Tan <yilintan47@gmail.com>
2026-01-06 19:10:46 +0800

    initial stack added

---
+++ stack_utils1.c
@@ -0,0 +1,55 @@
+void swap_stack (t_list **stack)
+{
+	t_list	*first;
+	t_list	*second;
+
+	if (!stack || !*stack || !**stack)
+		return;
+	first = *stack;
+	second = *stack -> next;
+	first -> next = second -> next;
+	second -> next = first;
+	*stack = second;
+}
+
+void reverse_rotate_stack (t_list **stack)
+{
+	// bottom comes to top, last becomes first 
+	t_list *last;
+	t_list *second_last 
+
+	if (!stack || !*stack || !**stack)
+		return;
+	while (second_last -> next -> next)
+		second_last = second_last -> next;
+	
+	last = second_last -> next;
+	second_last -> next = NULL;
+	last -> next = *stack;
+	*stack = last;
+
+}
+
+void rotate_stack (t_list **stack)
+{
+	// first element becomes the last one 
+	t_list *last;
+	t_list *second;
+	if (!stack || !*stack || !**stack)
+		return;
+	last = ft_getlastlist_bonus(stack);
+	second = *stack -> next;
+	last -> next = *stack;
+	*stack = second;
+}
+
+void push_stack (t_list **src, t_list **dest)
+{
+	t_list *move;
+	if (!stack || !*stack || !**stack)
+		return;
+	
+	move = *src;
+	*src = move -> next;
+	ft_addfrontlist_bonus (dest, move) // accounts for empty list 
+}
